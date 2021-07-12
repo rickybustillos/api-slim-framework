ef0d31943658d148836771ae6acb4bcf100970df
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-07-11 23:22:26 -0300

    adding database and eloquent ORM

--- index.php
+++ index.php
@@ -2,11 +2,30 @@
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 // Loading all vendor dependencies
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
+
+$container = $app->getContainer();
+$container['db'] = function(){
+  
+  $capsule = new Capsule;
+  $capsule->addConnection([
+      'driver' => 'mysql',
+      'host' => 'localhost',
+      'database' => 'slim',
+      'username' => 'root',
+      'password' => '',
+      'charset' => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix' => '',
+  ]);
+  $capsule->setAsGlobal();
+  $capsule->bootEloquent();
+
+};
 
 $app->group('/v1', function () {
 
